92c9470b1f93fb3f26f1a4a04904c6325eb4ef5d
clemsbe <110553572+clemsbe@users.noreply.github.com>
2022-11-07 13:40:36 +0100

    Initial commit

---
+++ php/formu.php
@@ -0,0 +1,14 @@
+<pre>
+		<form method="get" action="traitement.php">
+			
+			Marque            : <input type="text" name="inputMarque" /><br />
+			Prix maximum      : <input type="text" name="inputPrix" /><br />
+			Catégorie  : <select name="inputCategorie">
+											<option value="0">-- Catégories --</option>
+											<option value="Ballons">Ballons</option>
+											<option value="chaussures">Chaussures</option>
+										  </select>
+			<input type="submit" name="inputSubmit" value="Exécuter" />
+			<input type="reset" name="inputReset" value="Reset" />
+		</form>
+		</pre>
